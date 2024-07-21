b1d9b06e5305e5947fd3248d681493abc7ba047a
rendy25 <rendydwi38@gmail.com>
2024-07-21 11:23:36 +0700

    new-separator

---
+++ lib/moneyseparator.dart
@@ -0,0 +1,220 @@
+library moneyseparator;
+
+import 'package:flutter/services.dart';
+import 'package:function_tree/function_tree.dart';
+
+class InvalidExpressionException implements Exception {}
+
+extension NumberRounding on double {
+  String truncatePrecision(int precision) {
+    var res = toString();
+    var decimal = res.indexOf('.');
+    if (decimal == -1) {
+      return res;
+    }
+    var decimalPosition = decimal + precision + 1;
+    if (decimalPosition >= res.length) {
+      return res + ("0" * (decimalPosition - res.length));
+    }
+    return res.substring(0, decimalPosition);
+  }
+}
+
+class MoneyFormatter extends TextInputFormatter {
+  /// number of decimals allowed, defaults to 2
+  final int precision;
+
+  /// character separing the thousands, defaults to space
+  final String thousandSeparator;
+
+  /// caracter so separate the decimal digits, default to a period, a lot of language are using a comma though
+  /// You can get the decimal from the local by calling numberFormatSymbols[language]?.DECIMAL_SEP
+  final String decimalSeparator;
+
+  MoneyFormatter({
+    this.decimalSeparator = '.',
+    this.thousandSeparator = ' ',
+    this.precision = 2,
+  }) {
+    if (decimalSeparator == thousandSeparator) {
+      throw Exception(
+          "decimalSeparator cannot be the same as thousandSeparator");
+    }
+  }
+
+  String applyMask(double value) {
+    var fixedPrecision = value.toStringAsFixed(precision + 1);
+
+    // we convert numbers like 0.213 into 0.23
+    // this is due to the fact that there's a two digit limit
+    // so if the user touches another digit, we expect to replace the last one
+    if (fixedPrecision[fixedPrecision.length - 1] != '0') {
+      var newVal = value.truncatePrecision(precision);
+      value = double.parse(newVal.substring(0, newVal.length - 1) +
+          fixedPrecision[fixedPrecision.length - 1]);
+    }
+
+    List<String?> textRepresentation = value
+        .toStringAsFixed(precision)
+        .replaceAll('.', '')
+        .split('')
+        .reversed
+        .toList();
+
+    textRepresentation.insert(precision, decimalSeparator);
+
+    for (var i = precision + 4; true; i = i + 4) {
+      if (textRepresentation.length > i) {
+        textRepresentation.insert(i, thousandSeparator);
+      } else {
+        break;
+      }
+    }
+
+    var txt = textRepresentation.reversed.join('');
+    return txt.replaceFirst("${decimalSeparator}00", '');
+  }
+
+  double numberValue(String val) {
+    if (val == "") {
+      return 0;
+    }
+
+    late num interpreted;
+    try {
+      interpreted = val
+          .replaceAll(thousandSeparator, '')
+          .replaceFirst(decimalSeparator, '.')
+          .replaceFirst(',', '.')
+          .interpret();
+    } catch (e) {
+      throw InvalidExpressionException();
+    }
+
+    return interpreted.toDouble();
+  }
+
+  TextEditingValue formatEditUpdateCalculate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    var newText = newValue.text
+        .replaceAll('--', '')
+        .replaceAll('+-', '-')
+        .replaceAll(' ', '');
+    var difference = newText.length - oldValue.text.length;
+
+    // no changes
+    if (newText == newValue.text) {
+      return newValue;
+    }
+
+    return TextEditingValue(
+      text: newText,
+      selection: TextSelection.collapsed(
+          offset: newValue.selection.baseOffset + difference - 1),
+      composing: TextRange.empty,
+    );
+  }
+
+  bool containsCalculations(TextEditingValue value) {
+    return value.text.contains('-') ||
+        value.text.contains('+') ||
+        value.text.contains('(') ||
+        value.text.contains('*') ||
+        value.text.contains('/') ||
+        value.text.contains(')');
+  }
+
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.text == "") {
+      return const TextEditingValue(
+        text: "",
+        selection: TextSelection.collapsed(offset: 0),
+        composing: TextRange.empty,
+      );
+    }
+    if (newValue.text == "0") {
+      return const TextEditingValue(
+        text: "0",
+        selection: TextSelection.collapsed(offset: 1),
+        composing: TextRange.empty,
+      );
+    }
+
+    if (containsCalculations(newValue)) {
+      return formatEditUpdateCalculate(oldValue, newValue);
+    }
+
+    // too many separators
+    if (decimalSeparator.allMatches(newValue.text).length > 1) {
+      return oldValue;
+    }
+
+    // we deleted a space
+    if (oldValue.text != newValue.text &&
+        oldValue.text.replaceAll(' ', '') ==
+            newValue.text.replaceAll(' ', '')) {
+      var newVal =
+          oldValue.text.substring(0, oldValue.selection.baseOffset - 2) +
+              oldValue.text.substring(oldValue.selection.baseOffset - 1);
+      var formattedVal = applyMask(numberValue(newVal));
+      return TextEditingValue(
+          text: formattedVal,
+          selection: TextSelection.collapsed(
+              offset: oldValue.selection.baseOffset - 2));
+    }
+
+    String masked = applyMask(numberValue(newValue.text));
+
+    // no changes
+    if (masked == newValue.text) {
+      return newValue;
+    }
+
+    var spacesBeforeCursor = 0;
+    var oldCursor = oldValue.selection.baseOffset;
+    for (var i = 0; i < oldCursor; i++) {
+      if (oldValue.text[i] == thousandSeparator) {
+        spacesBeforeCursor++;
+      }
+    }
+    oldCursor -= spacesBeforeCursor;
+    spacesBeforeCursor = 0;
+
+    var newCursor = newValue.selection.baseOffset;
+    for (var i = 0; i < newCursor; i++) {
+      if (newValue.text[i] == ' ') {
+        spacesBeforeCursor++;
+      }
+    }
+    newCursor -= spacesBeforeCursor;
+
+    var spacesToAdd = 0;
+    var _charCount = 0;
+    for (var i = 0; i < masked.length; i++) {
+      if (masked[i] == ' ') {
+        spacesToAdd++;
+      } else {
+        _charCount++;
+        if (_charCount == newCursor) {
+          break;
+        }
+      }
+    }
+
+    var offset = newCursor + spacesToAdd;
+
+    if (newValue.text.endsWith(decimalSeparator)) {
+      masked += decimalSeparator;
+    } else if (newValue.text.endsWith('${decimalSeparator}0') &&
+        newCursor - oldCursor >= 0) {
+      masked += '${decimalSeparator}0';
+    }
+
+    return TextEditingValue(
+        text: masked,
+        selection: TextSelection.collapsed(
+            offset: offset > masked.length ? masked.length : offset));
+  }
+}
---
+++ test/moneyseparator_test.dart
@@ -0,0 +1,287 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:moneyseparator/moneyseparator.dart';
+
+void main() async {
+  test('truncate precision', () {
+    expect(55.449.truncatePrecision(2), '55.44');
+    expect(55.4.truncatePrecision(2), '55.40');
+    expect(55.4.truncatePrecision(5), '55.40000');
+  });
+
+  test('no value should nothing with the cursor at the end', () {
+    var res = MoneyFormatter()
+        .formatEditUpdate(const TextEditingValue(), const TextEditingValue());
+    expect(res.text, '');
+    expect(res.selection.baseOffset, 0, reason: 'cursor is at the end');
+    expect(res.selection.extentOffset, 0, reason: 'no selection');
+  });
+
+  test('deleting zero works', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(text: "0"), const TextEditingValue());
+    expect(res.text, '');
+    expect(res.selection.baseOffset, 0, reason: 'cursor is at the end');
+    expect(res.selection.extentOffset, 0, reason: 'no selection');
+  });
+
+  test('deleting a space reformats the input: 123 |456 => 12| 456', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "123 456",
+            selection: TextSelection(baseOffset: 4, extentOffset: 4)),
+        const TextEditingValue(
+            text: "123456",
+            selection: TextSelection(baseOffset: 3, extentOffset: 3)));
+    expect(res.text, '12 456');
+    expect(res.selection.baseOffset, 2, reason: 'cursor is after the two');
+    expect(res.selection.extentOffset, 2, reason: 'no selection');
+  });
+
+  test('three digits are converted to two digits (1.213 => 1.23)', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "1.21",
+            selection: TextSelection(baseOffset: 4, extentOffset: 4)),
+        const TextEditingValue(
+            text: "1.213",
+            selection: TextSelection(baseOffset: 5, extentOffset: 5)));
+    expect(res.text, '1.23');
+    expect(res.selection.baseOffset, 4, reason: 'cursor is still at the end');
+    expect(res.selection.extentOffset, 4, reason: 'no selection');
+  });
+
+  test('does not round up after three digits (1.299 => 1.29)', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "1.29",
+            selection: TextSelection(baseOffset: 4, extentOffset: 4)),
+        const TextEditingValue(
+            text: "1.299",
+            selection: TextSelection(baseOffset: 5, extentOffset: 5)));
+    expect(res.text, '1.29');
+    expect(res.selection.baseOffset, 4, reason: 'cursor is still at the end');
+    expect(res.selection.extentOffset, 4, reason: 'no selection');
+  });
+
+  test('works for comma insertion', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "1",
+            selection: TextSelection(baseOffset: 1, extentOffset: 1)),
+        const TextEditingValue(
+            text: "1.",
+            selection: TextSelection(baseOffset: 2, extentOffset: 2)));
+
+    expect(res.text, '1.');
+    expect(res.selection.baseOffset, 2, reason: 'cursor is still at the end');
+    expect(res.selection.extentOffset, 2, reason: 'no selection');
+  });
+
+  test('works for digit after comma insertion', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "1.",
+            selection: TextSelection(baseOffset: 2, extentOffset: 2)),
+        const TextEditingValue(
+            text: "1.0",
+            selection: TextSelection(baseOffset: 3, extentOffset: 3)));
+
+    expect(res.text, '1.0');
+    expect(res.selection.baseOffset, 3, reason: 'cursor is still at the end');
+    expect(res.selection.extentOffset, 3, reason: 'no selection');
+  });
+
+  test('formats the text and keeps the cursor', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "123456",
+            selection: TextSelection(baseOffset: 3, extentOffset: 3)),
+        const TextEditingValue(
+            text: "1239456",
+            selection: TextSelection(baseOffset: 4, extentOffset: 4)));
+
+    expect(res.text, '1 239 456');
+    expect(res.selection.baseOffset, 5,
+        reason: 'cursor moved due to adding a space when formatting');
+    expect(res.selection.extentOffset, 5, reason: 'no selection');
+  });
+
+  test('works with i18n', () {
+    var text = MoneyFormatter(decimalSeparator: ',');
+    var res = text.formatEditUpdate(
+        const TextEditingValue(
+            text: "11114,32",
+            selection: TextSelection(baseOffset: 8, extentOffset: 8)),
+        const TextEditingValue(
+            text: "11114,323",
+            selection: TextSelection(baseOffset: 9, extentOffset: 9)));
+
+    expect(res.text, '11 114,33');
+    expect(res.selection.baseOffset, 9);
+    expect(res.selection.extentOffset, 9);
+    expect(text.numberValue(res.text), 11114.33);
+  });
+
+  test('deleting caracter works', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "3.0",
+            selection: TextSelection(baseOffset: 3, extentOffset: 3)),
+        const TextEditingValue(
+            text: "3.",
+            selection: TextSelection(baseOffset: 2, extentOffset: 2)));
+    expect(res.text, "3.");
+    expect(res.selection.baseOffset, 2);
+    expect(res.selection.extentOffset, 2);
+  });
+
+  test('formatting after caracter deletion works', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "123 456.21",
+            selection: TextSelection(baseOffset: 6, extentOffset: 6)),
+        const TextEditingValue(
+            text: "123 46.21",
+            selection: TextSelection(baseOffset: 5, extentOffset: 5)));
+    expect(res.text, '12 346.21');
+    expect(res.selection.baseOffset, 5);
+    expect(res.selection.extentOffset, 5);
+  });
+
+  test('deleting caracter removes useless comma 0', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "58.09",
+            selection: TextSelection(baseOffset: 5, extentOffset: 5)),
+        const TextEditingValue(
+            text: "58.0",
+            selection: TextSelection(baseOffset: 4, extentOffset: 4)));
+    expect(res.text, '58');
+    expect(res.selection.baseOffset, 2);
+    expect(res.selection.extentOffset, 2);
+  });
+
+  test('inserting multiple chars at once', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "58.09",
+            selection: TextSelection(baseOffset: 1, extentOffset: 1)),
+        const TextEditingValue(
+            text: "50008.09",
+            selection: TextSelection(baseOffset: 4, extentOffset: 4)));
+    expect(res.text, "50 008.09");
+    expect(res.selection.baseOffset, 5);
+    expect(res.selection.extentOffset, 5);
+  });
+
+  test('works for digit after comma insertion', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "0",
+            selection: TextSelection(baseOffset: 1, extentOffset: 1)),
+        const TextEditingValue(
+            text: "0.",
+            selection: TextSelection(baseOffset: 2, extentOffset: 2)));
+
+    expect(res.text, '0.');
+    expect(res.selection.baseOffset, 2, reason: 'cursor is still at the end');
+    expect(res.selection.extentOffset, 2, reason: 'no selection');
+  });
+
+  // negative tests
+  test('inserts negative ', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "5",
+            selection: TextSelection(baseOffset: 0, extentOffset: 0)),
+        const TextEditingValue(
+            text: "-5",
+            selection: TextSelection(baseOffset: 1, extentOffset: 1)));
+
+    expect(res.text, '-5');
+    expect(res.selection.baseOffset, 1,
+        reason: 'cursor next to negative sign as normal');
+    expect(res.selection.extentOffset, 1, reason: 'no selection');
+  });
+
+  test('deletes double negative', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "-5",
+            selection: TextSelection(baseOffset: 1, extentOffset: 1)),
+        const TextEditingValue(
+            text: "--5",
+            selection: TextSelection(baseOffset: 2, extentOffset: 2)));
+    expect(res.text, '5');
+    expect(res.selection.baseOffset, 0);
+  });
+
+  test('deletes useless plus', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "-5",
+            selection: TextSelection(baseOffset: 1, extentOffset: 1)),
+        const TextEditingValue(
+            text: "+-5",
+            selection: TextSelection(baseOffset: 2, extentOffset: 2)));
+    expect(res.text, '-5');
+    expect(res.selection.baseOffset, 1);
+  });
+
+  test('inserts negative', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "1234",
+            selection: TextSelection(baseOffset: 2, extentOffset: 2)),
+        const TextEditingValue(
+            text: "12-34",
+            selection: TextSelection(baseOffset: 3, extentOffset: 3)));
+
+    expect(res.text, '12-34', reason: 'allows the negative sign');
+    expect(res.selection.baseOffset, 3, reason: 'cursor works as normal');
+    expect(res.selection.extentOffset, 3, reason: 'no selection');
+  });
+
+  test('removes all spaces from calculations', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "1 234",
+            selection: TextSelection(baseOffset: 5, extentOffset: 5)),
+        const TextEditingValue(
+            text: "1 234-",
+            selection: TextSelection(baseOffset: 6, extentOffset: 6)));
+
+    expect(res.text, '1234-', reason: 'no spaces for calculations');
+    expect(res.selection.baseOffset, 5, reason: 'cursor should be at the end');
+  });
+
+  test('inserts a plus sign when typing', () {
+    var res = MoneyFormatter().formatEditUpdate(
+        const TextEditingValue(
+            text: "1 234",
+            selection: TextSelection(baseOffset: 5, extentOffset: 5)),
+        const TextEditingValue(
+            text: "1 234+",
+            selection: TextSelection(baseOffset: 6, extentOffset: 6)));
+
+    expect(res.text, '1234+',
+        reason: 'plus is inserted normally and spaces deleted');
+    expect(res.selection.baseOffset, 5, reason: 'cursor should be at the end');
+  });
+
+  test('adding a second digit separator', () {
+    var text = MoneyFormatter(decimalSeparator: ',');
+    var res = text.formatEditUpdate(
+        const TextEditingValue(
+            text: "11,1",
+            selection: TextSelection(baseOffset: 4, extentOffset: 4)),
+        const TextEditingValue(
+            text: "11,1,",
+            selection: TextSelection(baseOffset: 5, extentOffset: 5)));
+
+    expect(res.text, '11,1', reason: 'did not change');
+    expect(res.selection.baseOffset, 4, reason: 'cursor should be at the end');
+  });
+}
